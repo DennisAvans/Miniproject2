8e54493125ef9fca590c4619f05ce7ab68837e3f
scriptdwarf <scriptdwarf@gmail.com>
2015-01-09 15:49:02 +0100

    ADDED: server and stuffz

---
+++ Miniproject/PizzaServer/Klant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaServer
+{
+    public class Klant
+    {
+        private string adres;
+        private string username;
+        private string password;
+
+        public Klant(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public bool autheticate(string password)
+        {
+            if (password.Equals(this.password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string getUserName()
+        {
+            return username;
+        }
+
+    }
+}
--- Miniproject/PizzaServer/Server.cs
+++ Miniproject/PizzaServer/Server.cs
@@ -17,6 +17,7 @@ namespace PizzaServer
     {
         private TcpClient incomingClient;
         private System.Timers.Timer updateTimer;
+        private static KlantDatabase KDB;
 
         public Server()
         {
@@ -24,6 +25,11 @@ namespace PizzaServer
             //updateTimer = new System.Timers.Timer(1000);
             //updateTimer.Elapsed += updateURLs;
             //updateTimer.Start();
+
+            
+
+            KDB = new KlantDatabase();
+            KDB.load();
       
             TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 1330);
             listener.Start();
@@ -44,6 +50,7 @@ namespace PizzaServer
         private void HandleClientThread(object obj)
         {
             TcpClient client = obj as TcpClient;
+            Klant klant;
 
             Console.WriteLine("Connection found!");
             while (true)
@@ -62,28 +69,22 @@ namespace PizzaServer
 
                 Console.WriteLine("\nResponse from client: {0}", received);
 
-                string extension = received.Substring(received.Length - 3, 3);
+                string[] splitted = received.Split('@');
                 byte[] bytes = null;
-                switch (extension)
+                switch (splitted[0])
                 {
 
-                    case "Connect":
-                    case "Disconnect":
-                        bytes = Encoding.Unicode.GetBytes(UpdateFeed(received)); //bytes = Encoding.Unicode.GetBytes(downloader.GetFeedItems(received));
+                    case "Con": handleConnect();
                         break;
-                    case "ForceDisconnect":
+                    case "FDC":
                         Console.WriteLine("client has disconnected");
                         client.Close();
                         return;
-                    case "forwardGPS":
-                        bytes = Encoding.Unicode.GetBytes(HandleDataRequest(received));
+                    case "GPS":
                         break;
-                    case "Register":
-                        string[] splitted = received.Split('@');
-                        int index = int.Parse(splitted[1]);
-                        bytes = HandleLoadMoreRequest(splitted[0], index);
-                        break;
-                    case
+                    case "Reg": // get string as "Reg@Username@password"
+                        Klant k = new Klant(splitted[1], splitted[2]);
+                        break; 
                     default:
                         bytes = Encoding.Unicode.GetBytes(UpdateFeed(received));
                         break;
@@ -94,121 +95,15 @@ namespace PizzaServer
             }
         }
 
-        private string UpdateFeed(string feedURL)
+        private void handleConnect()
         {
-            if (!urlIO.exist(feedURL))
+            klant = KDB.find(splitted[1]);
+            if (klant != null)
             {
-                urlIO.add(feedURL);
-                urlIO.SaveURLs();
-                Console.WriteLine("New URL added!");
-                newFeedAdded = true;
-            }
-
-            using (XMLDownloader downloader = new XMLDownloader())
-            {
-                // nieuwe items die je binnen haalt
-                List<FeedItem> newItems = downloader.ConvertToFeedItems(feedURL);
-                if (newItems == null)
+                if (!klant.autheticate(splitted[2]))
                 {
-                    return "OK";
+                    klant = null;
                 }
-
-                // verwijder alle tekens behalve de alphanumerieke
-                string filename = Regex.Replace(downloader._title, @"[^A-Za-z0-9]+", string.Empty);
-
-                // vul de arraylist met de nieuwe items die binnen komen
-                foreach (FeedItem item in newItems)
-                {
-                    // inladen van de totale feed van de sitenaam
-                    objectIO.LoadFeeds(filename);
-
-                    bool feedItemAlreadyExists = false;
-                    foreach (FeedItem existingItem in objectIO.feedItems)
-                    {
-                        // check of de nieuwe item al bestaat in de arraylist met alle feeditems
-                        // contains vergelijkt het object (?ofso?): het werkt niet, dus je vergelijkt alle titls en ids van de FeedItems die al bestaan met
-                        // de titel en de id van de items die je ophaalt
-                        if ((existingItem.Title.Equals(item.Title)) && (existingItem.ID.Equals(item.ID)))
-                        {
-                            feedItemAlreadyExists = true;
-                            break;
-                        }
-                    }
-                    if (!feedItemAlreadyExists)
-                    {
-                        objectIO.add(item);
-                        objectIO.SaveFeeds(filename);
-                        Console.WriteLine("Item added in " + filename + "!");
-                    }
-                    // clearen omdat het problemen geeft
-                    objectIO.feedItems.Clear();
-                }
-                return "OK";
-            }
-        }
-
-        private byte[] HandleLoadMoreRequest(string tabname, int index)
-        {
-
-            string filename = Regex.Replace(tabname, @"[^A-Za-z0-9]+", string.Empty);
-            objectIO.LoadTempFeeds(filename, index);
-            MemoryStream fs = new MemoryStream();
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(fs, objectIO.tempFeedItems);
-            byte[] rval = fs.ToArray();
-            fs.Close();
-            objectIO.tempFeedItems.Clear();
-            return rval;
-        }
-
-        private string HandleDataRequest(string request)
-        {
-            return null;
-        }
-
-        private string HandleRSSrequest(string request)
-        {
-            string returnMessage = "RSS request received ty very much";
-            return null;
-        }
-
-        // update methode via timers
-        private void UpdateURLs(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            foreach (var url in urlIO._listOfURLs)
-            {
-                UpdateFeed(url);
-                Console.WriteLine("Updated " + url);
-            }
-            Console.WriteLine("\n");
-        }
-
-        // update methode via threads
-        private void UpdateAllFeeds(URLIO urlio)
-        {
-            while (true)
-            {
-                if (newFeedAdded)
-                {
-                    urlIO.LoadURLs();
-                    newFeedAdded = false;
-                }
-                try
-                {
-                    foreach (var url in urlIO._listOfURLs)
-                    {
-                        UpdateFeed(url);
-                        Console.WriteLine("Updated " + url);
-                        Thread.Sleep(0);
-                    }
-                    Console.WriteLine("\n");
-                }
-                catch (Exception e)
-                {
-                    continue;
-                }
-                Thread.Sleep(0);  // Prevent 100% CPU usage
-
             }
         }
     }
